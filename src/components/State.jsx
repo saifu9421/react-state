c3c508a60c08cdbfda59ace72497fa7c40cd89d3
saifu9421 <saifursaif905@gmail.com>
2025-05-23 19:02:22 +0600

    first commit

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,24 +1,88 @@
 import { useState } from "react";
 
 function State(){
-              console.log("Hello World");
-              
-            const[counter,setCounter] = useState(0);
-        
-             const handleIncrement = ()=>{
-                      setCounter(counter+1);
+               const [noteTitle,setNotesTitle] = useState("");
+               const [notes,setNotes] = useState([]);
+                const [editMode,setEditMode] =  useState(false);
+                const [editableNotes,setEditabileNotes] =  useState(null);
+
+            function handleInputData(e){
+                  setNotesTitle(e.target.value);                  
+           };
+
+               function handleForm(e){
+                   e.preventDefault();
+                       if(noteTitle.trim() === ""){
+                           return alert("Empty Input");
+                       };
+
+                        editMode?updateHandler():createHandler();
+               };
+
+                  const createHandler = ()=>{
+                            const obj ={
+                                   id:Date.now() +"",
+                                   title:noteTitle,
+                        };
+                       setNotes([obj,...notes]);
+                       setNotesTitle("");
+                  };
+
+                const handleDelete = (noteId) =>{
+                   const newDataList = notes.filter((item)=>{ 
+                            if(item.id !== noteId){
+                                return item;
+                            }
+                   });
+                    setNotes(newDataList);
+                };
+
+                const editHandeler = (note)=>{
+                          setEditMode(true);
+                           setEditabileNotes(note);
+                           setNotesTitle(note.title)
+                };
+
+             const updateHandler = ()=>{
+                     const newArray = notes.map(items=>{
+                             if(items.id === editableNotes.id){
+                                   return{
+                                         ...items,title:noteTitle
+                                   }
+                             }else{
+                                return items;
+                                
+                             }
+                     });
+                     setNotes(newArray);
+                      setEditMode(false);
+                      setNotesTitle("");
              };
-             const handleDecrement = ()=>{
-                      if(counter > 0){
-                          setCounter(counter-1)
-                      }
-             };
-              
+
       return (
             <>
-               <p>Counter {counter}</p>
-               <button onClick={handleIncrement}>Increment</button>
-               <button onClick={handleDecrement}>Decrement</button>
+                <div className="App">
+                        <form onSubmit={handleForm}>
+                    <input type="text" name="" id="" value={noteTitle}  onChange={handleInputData}/>
+                      <button type="submit">{editMode ? `Update Data`:'Add Data'}</button>
+                        </form>
+
+                        <div className="notes-items">
+                             <h2>All Notes</h2>
+                              <ul>
+                                 {notes.map((data)=>{
+                                 return <>
+                                    <li key={data.id}>
+                                     <span> {data.title}</span>
+                                      <button onClick={()=>editHandeler(data)}>Edit</button>
+                                        <button onClick={()=>handleDelete(data.id)}>Delete</button> 
+                                        </li>
+                                        <hr />
+                                  </>
+                            })}
+                              </ul>
+                         </div>
+                </div>
              </> 
       );
 };
